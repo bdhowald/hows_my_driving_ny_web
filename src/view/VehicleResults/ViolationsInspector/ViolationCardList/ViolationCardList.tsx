74ad0c7e28aaa60c91ed15bd9344558ad5f16559
Brian Howald <bdhowald@gmail.com>
2025-03-26 04:13:09 -0400

    Add the divider value for violations without a description.

--- src/view/VehicleResults/ViolationsInspector/ViolationCardList/ViolationCardList.tsx
+++ src/view/VehicleResults/ViolationsInspector/ViolationCardList/ViolationCardList.tsx
@@ -90,7 +90,7 @@ const ViolationCardList = ({
           .toString()
         break
       case Sort.KIND:
-        dividerValue = violation.humanizedDescription
+        dividerValue = violation.humanizedDescription ?? 'No Description Available'
         break
       case Sort.LOCATION: {
         const borough = violation.getBorough()
