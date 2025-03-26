cccb91abba4d21af8c328568d68fa98a14d9aaa9
Brian Howald <bdhowald@gmail.com>
2025-03-26 03:49:16 -0400

    Also allow for toggling off of new-style display via cookies.

--- src/view/Search/Search.tsx
+++ src/view/Search/Search.tsx
@@ -95,17 +95,20 @@ const Search = ({
       cookies[USE_NEW_STYLE_DISPLAY_COOKIE]
 
     const queryParamFeatureFlagEnabled =
-      queryParameters.get('useNewStyleDisplay')
+      queryParameters.get('useNewStyleDisplay') === 'true'
 
-    if (!useNewStyleDisplayCookiePresent || queryParamFeatureFlagEnabled) {
+    const queryParamFeatureFlagDisabled =
+      queryParameters.get('useNewStyleDisplay') === 'false'
+
+    if (!useNewStyleDisplayCookiePresent || queryParamFeatureFlagEnabled || queryParamFeatureFlagDisabled) {
       // 10% of sessions are in experimental group (plus some internal testers)
       // 50% of sessions are in control group
       // 40% of sessions are available for progressive rollout
       const randomVariable = Math.random()
       const inExperimentalGroup =
-        randomVariable * 10 > 9.0 || queryParamFeatureFlagEnabled === 'true'
+        randomVariable * 10 > 9.0 || queryParamFeatureFlagEnabled
       const inControlGroup =
-        randomVariable * 10 < 5.0 && !queryParamFeatureFlagEnabled
+        (randomVariable * 10 < 5.0 && !queryParamFeatureFlagEnabled) || queryParamFeatureFlagDisabled
 
       const inReserveGroup = !inControlGroup && !inExperimentalGroup
 
