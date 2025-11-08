c08edca62d0395c00748e6b3a59ada84a3cedc01
Brian Howald <bdhowald@gmail.com>
2025-11-08 12:42:10 -0500

    Don't add a space before already title-cased words.

--- src/utils/displayResults/convertCamelCaseToTitleCase/convertCamelCaseToTitleCase.test.ts
+++ src/utils/displayResults/convertCamelCaseToTitleCase/convertCamelCaseToTitleCase.test.ts
@@ -12,4 +12,8 @@ describe('convertCamelCaseToTitleCase', () => {
   it('should convert a camel-cased word to a title-cased word', () => {
     expect(convertCamelCaseToTitleCase('titleCaseMe')).toBe('Title Case Me')
   })
+
+  it('should leave title-cased words as is', () => {
+    expect(convertCamelCaseToTitleCase('Staten Island')).toBe('Staten Island')
+  })
 })
--- src/utils/displayResults/convertCamelCaseToTitleCase/convertCamelCaseToTitleCase.ts
+++ src/utils/displayResults/convertCamelCaseToTitleCase/convertCamelCaseToTitleCase.ts
@@ -1,5 +1,5 @@
 const convertCamelCaseToTitleCase = (original: string) => {
-  const converted = original.replace(/([A-Z])/g, ' $1')
+  const converted = original.replace(/([a-z])([A-Z])/g, '$1 $2')
   return converted.charAt(0).toUpperCase() + converted.slice(1)
 }
 
