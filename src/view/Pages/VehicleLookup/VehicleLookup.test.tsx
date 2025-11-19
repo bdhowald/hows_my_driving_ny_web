20fcccdf7779fb685336baebc475310e73f45315
Brian Howald <bdhowald@gmail.com>
2025-11-19 01:08:49 -0500

    Fix flaky test with added timeout time.

--- src/view/Pages/VehicleLookup/VehicleLookup.test.tsx
+++ src/view/Pages/VehicleLookup/VehicleLookup.test.tsx
@@ -704,7 +704,7 @@ describe('VehicleLookup', () => {
       await waitFor(() => {
         expect(scrollIntoViewFunction).not.toHaveBeenCalled()
       })
-    })
+    }, 12500)
 
     it('clicking on the close button of a lookup should remove it from the page', async () => {
       const performNewLookupSpy = jest.spyOn(
